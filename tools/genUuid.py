0203ec9ab2a2c5e1885b18e9a18fb3a3522ab12a
tranvantai2905 <tai.trantai58@hcmut.edu.vn>
2023-10-03 23:15:07 +0700

    refactor(dataset): add id

--- tools/genUuid.py
+++ tools/genUuid.py
@@ -1,39 +1,42 @@
 import json
 import uuid
 
+
 def is_valid_uuid(value):
-  try:
-    uuid_obj = uuid.UUID(value)
-    return True
-  except ValueError:
-    return False
+    try:
+        uuid_obj = uuid.UUID(value)
+        return True
+    except ValueError:
+        return False
+
 
 def main():
-  datasetBase = "./dataset/"
-  print("Input file name: ")
-  fileName = input()
-  if fileName == "":
-    fileName = datasetBase + "train_v1.json"
-  else:
-    fileName = datasetBase + fileName
-  
-  try:
-    with open(fileName, "r") as f:
-      trainObj = json.load(f)
-    dataTrains = trainObj["train"]["data"]
+    datasetBase = "./dataset/"
+    print("Input file name: ")
+    fileName = input()
+    if fileName == "":
+        fileName = datasetBase + "train_v1.json"
+    else:
+        fileName = datasetBase + fileName
 
-    uniqueId = ""
+    try:
+        with open(fileName, "rb") as f:
+            trainObj = json.load(f)
+        dataTrains = trainObj["train"]["data"]
 
-    for i in range(len(dataTrains)):
-      if not is_valid_uuid(dataTrains[i]["id"]):
-        uniqueId = str(uuid.uuid4())
-        trainObj["train"]["data"][i]["id"] = uniqueId
+        uniqueId = ""
 
-    with open(fileName, "w", encoding='utf-8') as f:
-      json.dump(trainObj, f, ensure_ascii = False, indent = 2) 
-  
-  except Exception as error:
-    raise error
-  
+        for i in range(len(dataTrains)):
+            if not is_valid_uuid(dataTrains[i]["id"]):
+                uniqueId = str(uuid.uuid4())
+                trainObj["train"]["data"][i]["id"] = uniqueId
+
+        with open(fileName, "w", encoding="utf-8") as f:
+            json.dump(trainObj, f, ensure_ascii=False, indent=2)
+
+    except Exception as error:
+        raise error
+
+
 if __name__ == "__main__":
-  main()
+    main()

45bf7145182fc9dfc608c324edbb83d8e89f875a
hoan7902 <hongcan792002@gmail.com>
2023-12-06 00:48:24 +0700

    handle cors

--- src/server.py
+++ src/server.py
@@ -19,6 +19,28 @@ def _check_question_type(c_key: int):
         return True
     return False
 
+@app.before_request
+async def before_request():
+    if request.method == 'OPTIONS':
+        return cors_after_request()
+
+@app.after_request
+async def after_request(response):
+    return cors_after_request(response)
+
+def cors_after_request(response=None):
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '86400',  # 24 hours
+    }
+    if response:
+        response.headers.extend(headers)
+    else:
+        return headers
+    return response
+
 @app.route('/api/question-types', methods=['GET'])
 def get_question_types():
     question_types = context_config.context_types
